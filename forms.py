b36d9b21ad91187ee7ae6639e6ff22f3d858f15b
Sean Gibson <seanthomasgibson@gmail.com>
2020-12-08 15:14:12 -0800

    Made species dropdown and added validators. Modified PetForm in forms.py.

--- forms.py
+++ forms.py
@@ -1,16 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, SelectField
+from wtforms.validators import URL, Optional, NumberRange
 
 class PetForm(FlaskForm):
     """
         Defines a form to create or add a pet
-    """
+    """    
     name = StringField("Name")
     
-    species = StringField("Species")
+    species = SelectField("Species", \
+        choices=[("cat", "cat"), ("dog", "dog"), ("porcupine", "porcupine")])
 
-    photo_url = StringField("Photo")
+    photo_url = StringField("Photo", validators=[URL(), Optional()])
 
-    age = IntegerField("Age")
+    age = IntegerField("Age", validators=[NumberRange(0, 30), Optional()])
 
     notes = StringField("Notes")
